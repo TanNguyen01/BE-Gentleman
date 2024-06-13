a87eabe331ed837eedef9e56941f1d1c79540113
thien <dothien.dvt.2508@gmail.com>
2024-06-13 16:13:08 +0700

    fix/product

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductsRequest;
 use App\Services\ProductService;
-use App\Traits\ApiResponseTrait;
-use Illuminate\Http\Request;
-use App\Http\Resources\ProductResource;
+use App\Traits\APIResponse;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
-    use ApiResponseTrait;
+    use APIResponse;
 
     protected $productService;
+
 
     public function __construct(ProductService $productService)
     {
@@ -21,79 +22,78 @@ class ProductController extends Controller
 
     public function index()
     {
-        try {
-            $products = $this->productService->getAllProducts();
-            return $this->successResponse(["products" => $products], 'Products retrieved successfully.');
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 500);
+        $products = $this->productService->getAllProducts();
+        return $this->responseSuccess(
+            __('Lấy danh sách thành công!'),
+            [
+                'product' => $products,
+            ]
+        );
+    }
+
+    public function store(ProductsRequest $request)
+    {
+        $productData = $request->all();
+        $variantsData = $request->input('variants', []);
+        $product = $this->productService->storeProductWithVariants($productData, $variantsData);
+        return $this->responseCreated(
+            __('Tao san pham thanh cong!'),
+            [
+                'product' => $product
+            ]
+        );
+    }
+
+    public function show(int $id)
+    {
+        $product = $this->productService->showProduct($id);
+        if (!$product) {
+            return
+                $this->responseNotFound(
+                    Response::HTTP_NOT_FOUND,
+                    __('khong tim thay san pham!')
+
+                );
+        } else {
+            return $this->responseSuccess(
+                __('lay san pham thanh cong!'),
+                [
+                    'product' => $product,
+                ]
+            );
         }
     }
 
-    public function store(Request $request)
+    public function update(ProductsRequest $request, $id)
     {
-        try {
-            $data = $request->validate([
-                'name' => 'required|string|unique:products',
-                'category_id' => 'required|exists:categories,id',
-                'brand' => 'nullable|string',
-                'description' => 'nullable|string',
-                'variants' => 'required|array',
-                'variants.*.price' => 'required|numeric',
-                'variants.*.quantity' => 'required|integer',
-                'variants.*.attribute_values' => 'required|array',
-                'variants.*.attribute_values.*.attribute_id' => 'required|exists:attributes,id',
-                'variants.*.attribute_values.*.value' => 'required|string',
-
-            ]);
-
-            $product = $this->productService->createProduct($data);
-            return $this->successResponse(new ProductResource($product), 201);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 400);
+        $data = $request->all();
+        $product = $this->productService->updateProduct($id, $data);
+        if (!$product) {
+            return $this->responseNotFound(
+                Response::HTTP_NOT_FOUND,
+                __('khong tim thay san pham !')
+            );
+        } else {
+            $product->update($data);
+            return $this->responseSuccess(
+                __('sua thanh cong'),
+                [
+                    'product' => $product
+                ]
+            );
         }
     }
 
-    public function show($id)
+    public function destroy(string $id)
     {
-        try {
-            $product = $this->productService->getProductById($id);
-            return $this->successResponse(new ProductResource($product));
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 404);
-        }
-    }
-
-    public function update(Request $request, $id)
-    {
-        try {
-            $data = $request->validate([
-                'name' => 'string|unique:products',
-                'category_id' => 'exists:categories,id',
-                'brand' => 'nullable|string',
-                'description' => 'nullable|string',
-                'variants' => 'array',
-                'variants.*.price' => 'numeric',
-                'variants.*.quantity' => 'integer',
-                'variants.*.attribute_values' => 'array',
-                'variants.*.attribute_values.*.attribute_id' => 'exists:attributes,id',
-                'variants.*.attribute_values.*.value' => 'string',
-                'variants.*.attributes.*.quantity' => 'required|integer',
-            ]);
-
-            $product = $this->productService->updateProduct($id, $data);
-            return $this->successResponse(new ProductResource($product));
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 400);
-        }
-    }
-
-    public function destroy($id)
-    {
-        try {
-            $this->productService->deleteProduct($id);
-            return $this->successResponse('Product deleted successfully');
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 400);
+        $product = $this->productService->destroyProduct($id);
+        if (!$product) {
+            return $this->responseNotFound(
+                Response::HTTP_NOT_FOUND,
+                __('khong tim thay bien the!')
+            );
+        } else {
+            return $this->responseDeleted(null, Response::HTTP_NO_CONTENT);
         }
     }
 }
--- app/Http/Controllers/API/VariantController.php
+++ app/Http/Controllers/API/VariantController.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\VariantResource;
+use App\Http\Requests\VariantsRequest;
 use App\Services\VariantService;
-use App\Traits\ApiResponseTrait;
-use Illuminate\Http\Request;
+use App\Traits\APIResponse;
+use Illuminate\Http\Response;
 
 class VariantController extends Controller
 {
-    use ApiResponseTrait;
+    use APIResponse;
 
     protected $variantService;
+
 
     public function __construct(VariantService $variantService)
     {
@@ -20,60 +22,82 @@ class VariantController extends Controller
 
     public function index()
     {
-        try {
-            $variants = $this->variantService->getAllVariants();
-            return $this->successResponse(['variants' => VariantResource::collection($variants)],'Get variant list successfully.');
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 500);
+        $variant = $this->variantService->getVariant();
+        return $this->responseSuccess(
+            __('Lấy danh sách thành công!'),
+            [
+                'data' => $variant,
+            ]
+        );
+    }
+
+    public function store(VariantsRequest $request)
+    {
+        $data = $request->all();
+        $variant = $this->variantService->storeVariant($data);
+        return $this->responseCreated(
+            __('Tao danh muc thanh cong!'),
+            [
+                'data' => $variant
+            ]
+        );
+    }
+
+    public function show(String $id)
+    {
+        $variant = $this->variantService->showVariant($id);
+        if (!$variant) {
+            return
+                $this->responseNotFound(
+                    Response::HTTP_NOT_FOUND,
+                    __('khong tim thay bien the!'),
+                    [
+                        'data' => $variant,
+                    ]
+                );
+        } else {
+            return $this->responseSuccess(
+                __('lay bien the thanh cong!'),
+                [
+                    'data' => $variant,
+                ]
+            );
         }
     }
 
-    public function store(Request $request)
+    public function update(VariantsRequest $request, $id)
     {
-        try {
-            $request->validate([
-                'name' => 'required|string|max:255',
-            ]);
-
-            $variant = $this->variantService->createVariant($request->all());
-            return new VariantResource($variant);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 400);
+        $data = $request->all();
+        $variant = $this->variantService->updateVariant($id, $data);
+        if (!$variant) {
+            return $this->responseNotFound(
+                Response::HTTP_NOT_FOUND,
+                __('khong tim thay bien the!'),
+                [
+                    'data' => $variant
+                ]
+            );
+        } else {
+            $variant->update($data);
+            return $this->responseSuccess(
+                __('sua thanh cong'),
+                [
+                    'data' => $variant
+                ]
+            );
         }
     }
 
-    public function show($id)
+    public function destroy(string $id)
     {
-        try {
-            $variant = $this->variantService->getVariantById($id);
-            return new VariantResource($variant);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 404);
-        }
-    }
-
-    public function update(Request $request, $id)
-    {
-        try {
-            $request->validate([
-                'name' => 'required|string|max:255',
-            ]);
-
-            $variant = $this->variantService->getVariantById($id);
-            $variant = $this->variantService->updateVariant($variant, $request->all());
-            return new VariantResource($variant);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 400);
-        }
-    }
-
-    public function destroy($id)
-    {
-        try {
-            $this->variantService->deleteVariant($id);
-            return $this->successResponse("Variant deleted successfully");
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(), 400);
+        $variant = $this->variantService->destroyVariant($id);
+        if (!$variant) {
+            return $this->responseNotFound(
+                Response::HTTP_NOT_FOUND,
+                __('khong tim thay bien the!')
+            );
+        } else {
+            return $this->responseDeleted(null, Response::HTTP_NO_CONTENT);
         }
     }
 }
